476516c1e9928db119b0d61ccbd668edd1bb090a
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2020-08-09 17:03:02 +0530

    fixed errors

--- YourName.py
+++ YourName.py
@@ -4,8 +4,8 @@ if os.path.exists("/data/data/com.termux/files/usr/etc/motd"):
 fname = input("please enter your first name:")
 lname = input("Please enter your last name:")
 file = open("/data/data/com.termux/files/usr/etc/bash.bashrc","w")
-code1 ="if  [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then\n    command_not_found_handle(){\n /data/data/com.termux/files/usr/libexec/termux/command-not-found ' $1'\n}\nfi\nPS1='\$'"
-code2 = "\nclear\necho -e '\e[1;31m' \nfiglet "+fname+"\necho -e '\e[1;33m'\ntoilet --filter border --metal "+lname+"\n echo -e '\e[1;32m' hello!! welcome "+fname+" "+lname
+code1 ="PS1='\$'"
+code2 = "\nclear\necho -e '\e[1;31m' \nfiglet "+fname+"\necho -e '\e[1;33m'\ntoilet --filter border --metal "+lname+"\n echo -e '\e[1;32m' hello!! welcome "+fname+" "+lname + " '\e[m'"
 file.write(code1+code2)
 file.close()
 ##This code is written by THE GENIUS HACKER. ALL RIGHTS ARE RESERVED https://fb.me/TheGeniusHacker 
