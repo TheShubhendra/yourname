09ae79a125afdd2388105185eeaf0e4367248c30
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2020-08-11 14:18:06 +0530

    Pkg checker added

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
-pkg install -y figlet toilet
+if [ $(dpkg-query -W -f='${Status}' figlet 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "Instaling Figlet"
+  apt-get install -y figlet
+fi
+if [ $(dpkg-query -W -f='${Status}' toilet 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "Installing Toilet"
+  apt-get -y toilet
+fi
 echo "Please enter your first name : "
 read fname
 echo "Please enter your last name : "
 read lname
+if [ -e /data/data/com.termux/files/user/etc/motd ]
+then
+   rm -f /data/data/com.termux/files/user/etc/motd 
+fi
 cat > /data/data/com.termux/files/usr/etc/bash.bashrc <<EOL
 PS1='\$ '
 clear
